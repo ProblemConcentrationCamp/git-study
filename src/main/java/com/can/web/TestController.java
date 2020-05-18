08288f2af609e73dde2d3d71aa0e251dd26320bb
LCN29 <CanLee529@gmail.com>
2020-05-18 16:52:33 +0800

    Revert "project init"
    This reverts commit ee391c6b2376e7dd4ccff9b0cb3466e3fd37d850.

--- src/main/java/com/can/web/TestController.java
+++ src/main/java/com/can/web/TestController.java
@@ -2,7 +2,6 @@ package com.can.web;
 
 import com.can.entity.User;
 import com.can.service.TestService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +16,6 @@ import javax.annotation.Resource;
  * @author canxin.li
  * @date 2020-05-11 20:41
  */
-@Slf4j
 @RestController
 public class TestController {
 
@@ -26,7 +24,6 @@ public class TestController {
 
     @GetMapping("/query/{userId}")
     public User queryById(@PathVariable int userId) {
-        log.info("参数---->{}", userId);
         return testService.queryById(userId);
     }
 }
