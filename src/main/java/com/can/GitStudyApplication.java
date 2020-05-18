b56754b2f27b2a1b6e6e058f76b12e93d8ff5eba
LCN29 <CanLee529@gmail.com>
2020-05-18 17:05:45 +0800

    delete the log

--- src/main/java/com/can/GitStudyApplication.java
+++ src/main/java/com/can/GitStudyApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitStudyApplication {
 
     public static void main(String[] args) {
-        log.info("application starting ...");
         SpringApplication.run(GitStudyApplication.class, args);
     }
 
